a3bb2acd6d86a980943545ec386b0b95a60df1ae
acer <acer@LAPTOP-CNIPVGVP>
2024-11-18 15:11:41 +0700

    Add project files.

---
+++ DMS_quary_data/Form1.cs
@@ -0,0 +1,43 @@
+using Microsoft.Data.SqlClient;
+using System.Data;
+namespace DMS_quary_data
+
+
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //idk
+        SqlConnection conn;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+
+        private void connect()
+        {
+            string server = @".\sqlexpress";
+            string db = "Minimart";
+            string strCon = string.Format(@"Data Source={0};Initial Catalog={1};" + "Integrated Security=True;Encrypt=False", server, db);
+            conn =new SqlConnection(strCon);
+            conn.Open();
+        }
+
+        private void disconnect()
+        {
+            conn.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connect();
+            string sql = "select * from Categories";
+            da = new SqlDataAdapter(sql,conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+    }
+}
