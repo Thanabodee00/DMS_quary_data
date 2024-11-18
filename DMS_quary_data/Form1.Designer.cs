030b67d80131b95a629d92066f97895f1d3fb885
Thanabodee00 <Knightcir@gmail.com>
2024-11-18 15:45:21 +0700

    update

--- DMS_quary_data/Form1.Designer.cs
+++ DMS_quary_data/Form1.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             dataGridView1 = new DataGridView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -44,11 +47,44 @@
             dataGridView1.Size = new Size(609, 323);
             dataGridView1.TabIndex = 0;
             // 
+            // button1
+            // 
+            button1.Location = new Point(12, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 1;
+            button1.Text = "พนักงาน";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(241, 12);
+            button2.Name = "button2";
+            button2.Size = new Size(138, 29);
+            button2.TabIndex = 2;
+            button2.Text = "หมวดหมู่สินค้า";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(504, 12);
+            button3.Name = "button3";
+            button3.Size = new Size(104, 29);
+            button3.TabIndex = 3;
+            button3.Text = "สินค้า";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(633, 395);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Controls.Add(dataGridView1);
             Name = "Form1";
             Text = "Form1";
@@ -60,5 +96,8 @@
         #endregion
 
         private DataGridView dataGridView1;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
--- DMS_quary_data/Form1.cs
+++ DMS_quary_data/Form1.cs
@@ -21,7 +21,7 @@ namespace DMS_quary_data
             string server = @".\sqlexpress";
             string db = "Minimart";
             string strCon = string.Format(@"Data Source={0};Initial Catalog={1};" + "Integrated Security=True;Encrypt=False", server, db);
-            conn =new SqlConnection(strCon);
+            conn = new SqlConnection(strCon);
             conn.Open();
         }
 
@@ -33,21 +33,37 @@ namespace DMS_quary_data
         private void Form1_Load(object sender, EventArgs e)
         {
             connect();
+            showData("select * from Categories");
             //string sql = "select * from Categories";
             //da = new SqlDataAdapter(sql,conn);
             //DataSet ds = new DataSet();
             //da.Fill(ds);
             //dataGridView1.DataSource = ds.Tables[0];
-           
+
         }
 
-        private void showData()
+        private void showData(String sql)
         {
-            string sql = "select * from Categories";
+            //string sql = "select * from Categories";
             da = new SqlDataAdapter(sql, conn);
             DataSet ds = new DataSet();
             da.Fill(ds);
             dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showData("SELECT EmployeeID,Title+FirstName+' '+lastname EmpName, Position from Employees");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            showData("select * from Categories ");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            showData("select * from Products");
         }
     }
 }
